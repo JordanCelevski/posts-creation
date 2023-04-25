841b27bc7fdc491f99c09419253ccc0d9f04adf4
Jordan <38318908+JordanCelevski@users.noreply.github.com>
2023-04-25 15:14:40 +0200

    post creation app with firebase and angular

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,50 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { map } from 'rxjs';
+import { Post } from './shared/posts.model';
+
+// type PostData = {
+//   title: string;
+//   content: string;
+// };
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'posts-creation';
+export class AppComponent implements OnInit {
+  loadedPosts: Post[] = [];
+
+  constructor(private http: HttpClient) {}
+
+  ngOnInit() {}
+
+  // { title: string; content: string }
+  onCreatePost(postData: Post) {
+    // Send Http request
+    console.log(postData);
+    //kako tip se stava responsot
+    this.http.post<{name: string}>('https://recipes-project-2995d-default-rtdb.firebaseio.com/posts.json', postData).subscribe(responseData => console.log(responseData));
+  }
+
+  onFetchPosts() {
+    this.http.get<{[key:string]: Post}>('https://recipes-project-2995d-default-rtdb.firebaseio.com/posts.json')
+    .pipe(map((responseData: {[key:string]: Post}) => {
+      const postsArray: Post[] = [];
+      for(const key in responseData) {
+        const value = responseData[key];
+        postsArray.push({...value, id: key});
+      }
+      return postsArray;
+    }))
+    .subscribe(
+      posts => this.loadedPosts = posts
+    );
+  }
+
+  onClearPosts() {
+    // Send Http request
+  }
+   
 }
---
+++ src/envinronments/environment.prod.ts
@@ -0,0 +1,4 @@
+export const environment = {
+  production: true,
+  fetchApiKey: '12312312312312'
+};
